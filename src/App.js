3e839848a0cf6d52248edb70074f38f6b2b40a43
pratik1802 <114296841+pratik1802@users.noreply.github.com>
2023-04-02 13:49:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import UseApi from './UseEffect/UseApi';
+
+
+function App() {
+
+
+
+  
+  
+  return (
+    <>
+    <div className="App">
+     
+    <UseApi/>
+    {/* <UseApi/> */}
+
+    </div>
+    </>
+  );
+}
+
+export default App;
