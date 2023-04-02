f45fc7f57e5def4042b9345ef13b2cfa1c6f3c4c
Patrick Kenneally <36610246+ptekspy@users.noreply.github.com>
2023-04-02 13:18:57 +0100

    Removed unneeded template tag for loop key error

--- src/UseEffect/UseApi.js
+++ src/UseEffect/UseApi.js
@@ -28,25 +28,23 @@ const UseApi = () => {
                     {
                         users.map((curEle) => {
                             return (
-                                <>
-                                    <div className="col-10 col-md-4 mt-5" key={curEle.id}>
-                                        <div className="card p-2">
-                                            <div className="d-flex align-items-center">
-                                                <div className="image"><img src={curEle.thumbnailUrl} className='rounded' width="155" /></div>
-                                                <div className="ml-3 w-100">
-                                                    <h4 className='mb-0 mt-0 textLeft'></h4><span className='textLeft'>{curEle.title}</span>
-                                                    <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
-                                                        <div className="d-flex flex-column"><span className='articles'>Articles</span><span className='number1'>38</span>
-                                                            <div className="d-flex flex-column"><span className='followers'>followers</span><span className='number2'>938</span>
-                                                                <div className="d-flex flex-column"><span className='rating'>Rating</span><span className='number3'>9.8</span></div>
-                                                            </div>
+                                <div className="col-10 col-md-4 mt-5" key={curEle.id}>
+                                    <div className="card p-2">
+                                        <div className="d-flex align-items-center">
+                                            <div className="image"><img src={curEle.thumbnailUrl} className='rounded' width="155" /></div>
+                                            <div className="ml-3 w-100">
+                                                <h4 className='mb-0 mt-0 textLeft'></h4><span className='textLeft'>{curEle.title}</span>
+                                                <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
+                                                    <div className="d-flex flex-column"><span className='articles'>Articles</span><span className='number1'>38</span>
+                                                        <div className="d-flex flex-column"><span className='followers'>followers</span><span className='number2'>938</span>
+                                                            <div className="d-flex flex-column"><span className='rating'>Rating</span><span className='number3'>9.8</span></div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </>
+                                </div>
                             )
                         })
 
